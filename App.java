22c9cf603bd06b7aea6f1770c0872f5b7b2141d6
Lauren <lhanna7@ford.com>
2022-05-23 14:43:58 -0700

    add solution

--- App.java
+++ App.java
@@ -1,6 +1,18 @@
 class App {
-  public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+  public static void main(String args[]) {
+
+    int VIN = 1234567;
+    String makeModel = "Ford Escape";
+    String color = "White";
+    Boolean towPkg = false;
+    int odo = 27_863;
+    int price = 28_000;
+    char quality = 'A';
+
+    String vehicleInfo = String.format(
+        "Vin: %d\nMake/Model: %s\nColor: %s\nTow Package: %s\nOdometer: %d\nPrice: %d\nQuality: %c",
+        VIN, makeModel, color, towPkg, odo, price, quality);
+
+    System.out.println(vehicleInfo);
   }
 }
